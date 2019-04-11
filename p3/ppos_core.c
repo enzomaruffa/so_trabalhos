783afc5b684251ee520e35d98be8afcad1b8a86b
ENZO MARUFFA MOREIRA <emm17@inf.ufpr.br>
2019-04-11 18:52:59 -0300

    p3 funcionanod de novo. falta só fazer a desalocação de memória apropriadamente - que eu falhei

--- p3/ppos_core.c
+++ p3/ppos_core.c
@@ -9,11 +9,10 @@
 int last_task_id = 0;
 
 task_t *main_task;
-int has_switched_task = 0;
-
 task_t *dispatcher_task;
 task_t *current_task;
 task_t *previous_task = NULL;
+task_t *to_be_next_task = NULL; //used when task exits
 
 task_t **dispatcher_active_tasks;
 int userTasks = 0;
@@ -23,12 +22,16 @@ int userTasks = 0;
 
 task_t *scheduler() 
 {
-    if (previous_task && previous_task != main_task) {
+    if (to_be_next_task != NULL) { //se alguma tarefa foi destruida, usa o antigo next
+        return to_be_next_task;
+    } 
+    else if (previous_task && previous_task != main_task) //se tem uma tarefa anterior e não é a main, usa a proxima
+    {
         return previous_task->next;
     }
     else
     {
-        return *(dispatcher_active_tasks);
+        return *(dispatcher_active_tasks); //se não tem indicação, executa a primeira da lista de tarefas
     }
 }
 
@@ -38,7 +41,12 @@ void dispatcher_body () // dispatcher é uma tarefa
     task_t *next = NULL;
     while ( userTasks > 0 )
     {
+        next = NULL;
         next = scheduler() ;  // scheduler é uma função
+
+        if (to_be_next_task) 
+            to_be_next_task = NULL;
+
         if (next)
         {
             //... // ações antes de lançar a tarefa "next", se houverem
@@ -58,7 +66,6 @@ void create_dispatcher()
     dispatcher_active_tasks = (task_t **)malloc(sizeof(task_t *));
     (*dispatcher_active_tasks) = NULL;
     userTasks = 0;
-
 }
 
 // ========================== Core ==============================
@@ -66,9 +73,18 @@ void create_dispatcher()
 // Inicializa o sistema
 void ppos_init () 
 {
+    /* cria a main task*/
+    main_task = malloc(sizeof(task_t));
+    task_create(main_task, NULL, NULL);
+    current_task = main_task;
+
     /* desativa o buffer da saida padrao (stdout), usado pela função printf */
     setvbuf (stdout, 0, _IONBF, 0) ;
     create_dispatcher() ;
+}
+
+void task_destroy(task_t *task) {
+    free(task->stack);
 }
 
 // Cria uma nova tarefa. Retorna um ID> 0 ou erro.
@@ -78,8 +94,8 @@ int task_create (task_t *task,			// descritor da nova tarefa
 {   
     getcontext (&task->context) ;
 
-    last_task_id++;
     task->id = last_task_id;
+    last_task_id++;
     task->stack = malloc(STACKSIZE) ;
 
     if (task->stack)
@@ -99,43 +115,30 @@ int task_create (task_t *task,			// descritor da nova tarefa
         makecontext (&task->context, (void*)(*start_func), 1, arg) ;
     }
 
-    if (dispatcher_active_tasks)
+    if (dispatcher_active_tasks) {
         userTasks++;
         queue_append((queue_t**) dispatcher_active_tasks, (queue_t*) task);
+    }
 
     return task->id;   
 }			
 
-void task_create_main ()			// descritor da nova tarefa
-{   
-    main_task = (task_t *) malloc(sizeof(task_t)) ;
-    getcontext (&main_task->context) ;
-
-    main_task->id = 0;
-    main_task->stack = malloc(STACKSIZE) ;
-    if (main_task->stack)
-    {
-        main_task->context.uc_stack.ss_sp = main_task->stack ;
-        main_task->context.uc_stack.ss_size = STACKSIZE ;
-        main_task->context.uc_stack.ss_flags = 0 ;
-        main_task->context.uc_link = 0 ;
-    }
-    else
-    {
-        perror ("Erro na criação da pilha: ") ;
-        exit (1) ;
-    }
-
-    current_task = main_task;
-    has_switched_task = 1;
-}	
-
 // Termina a tarefa corrente, indicando um valor de status encerramento
 void task_exit (int exitCode) 
 {
-    queue_remove((queue_t**) dispatcher_active_tasks, (queue_t*) current_task);
-    userTasks -= 1;
-    task_switch(main_task);
+    if (userTasks > 0 ) {
+        to_be_next_task = current_task->next; //salva a proxima tarefa
+
+        queue_remove((queue_t**) dispatcher_active_tasks, (queue_t*) current_task); //remove da lista de tarefas
+        userTasks -= 1; 
+        //task_destroy(current_task);
+        current_task = NULL; // seta tarefa atual como nula, assim não tenta atribuir contexto pra task que não existe
+
+        task_yield(); //cede pro dispatcher
+    } else {
+        //free dispatcher
+        task_switch(main_task);
+    }
 }
 
 // Tarefa solta o processador
@@ -147,14 +150,13 @@ void task_yield ()
 // alterna a execução para a tarefa indicada
 int task_switch (task_t *task) 
 {
-    if (!has_switched_task) { // first task switch
-        task_create_main();
-    }
-
     previous_task = current_task;
     current_task = task;
 
-    swapcontext (&(previous_task->context), &(task->context));
+    if (previous_task)
+        swapcontext (&(previous_task->context), &(task->context));
+    else
+        setcontext(&(task->context));
 
     return 0;
 }
