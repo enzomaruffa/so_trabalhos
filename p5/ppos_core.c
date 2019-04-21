e1369e23dbce7f8b97a4230c08a0e9b370880597
lucas.sampaio <lucassampaiowho@gmail.com>
2019-04-21 18:47:43 -0300

    cornos

--- p5/ppos_core.c
+++ p5/ppos_core.c
@@ -30,7 +30,7 @@ struct itimerval timer;
 
 // ========================== P5 ==============================
 void tick_handler() {
-    printf("tick handler\n");
+    // printf("tick handler\n");
     if (!current_task->is_user_task) {
         return;
     }
@@ -141,7 +141,7 @@ void ppos_init ()
     action.sa_handler = tick_handler;
     sigemptyset (&action.sa_mask) ;
     action.sa_flags = 0 ;
-    if (sigaction (SIGALRM, &action, 0) < 0)
+    if (sigaction (SIGVTALRM, &action, 0) < 0)
     {
         perror ("Erro em sigaction: ") ;
         exit (1) ;
@@ -149,12 +149,12 @@ void ppos_init ()
 
     // ajusta valores do temporizador
     timer.it_value.tv_usec = 0 ;      // primeiro disparo, em micro-segundos
-    timer.it_value.tv_sec  = 0 ;      // primeiro disparo, em segundos
+    timer.it_value.tv_sec  = 1 ;      // primeiro disparo, em segundos
     timer.it_interval.tv_usec = 1000 ;   // disparos subsequentes, em micro-segundos
-    timer.it_interval.tv_sec  = 0 ;   // disparos subsequentes, em segundos
+    timer.it_interval.tv_sec  = 0.001 ;   // disparos subsequentes, em segundos
 
     // arma o temporizador ITIMER_REAL (vide man setitimer)
-    if (setitimer (ITIMER_REAL, &timer, 0) < 0)
+    if (setitimer (ITIMER_VIRTUAL, &timer, 0) < 0)
     {
         perror ("Erro em setitimer: ") ;
         exit (1) ;
