0ebc4a8c9b1df1a28697c41e7730fa6b803babdf
lucas.sampaio <lucassampaiowho@gmail.com>
2019-04-21 19:55:02 -0300

    vou deixar assim por enquanto

--- p5/ppos_core.c
+++ p5/ppos_core.c
@@ -30,7 +30,6 @@ struct itimerval timer;
 
 // ========================== P5 ==============================
 void tick_handler() {
-    // printf("tick handler\n");
     if (!current_task->is_user_task) {
         return;
     }
@@ -148,12 +147,12 @@ void ppos_init ()
     }
 
     // ajusta valores do temporizador
-    timer.it_value.tv_usec = 0 ;      // primeiro disparo, em micro-segundos
-    timer.it_value.tv_sec  = 1 ;      // primeiro disparo, em segundos
+    timer.it_value.tv_usec = 1000 ;      // primeiro disparo, em micro-segundos
+    timer.it_value.tv_sec  = 0 ;      // primeiro disparo, em segundos
     timer.it_interval.tv_usec = 1000 ;   // disparos subsequentes, em micro-segundos
-    timer.it_interval.tv_sec  = 0.001 ;   // disparos subsequentes, em segundos
+    timer.it_interval.tv_sec  = 0 ;   // disparos subsequentes, em segundos
 
-    // arma o temporizador ITIMER_REAL (vide man setitimer)
+    // arma o temporizador
     if (setitimer (ITIMER_VIRTUAL, &timer, 0) < 0)
     {
         perror ("Erro em setitimer: ") ;
